82e34e03c4e672c3aa7b2da30414ce065ba7e0f0
Bill G <gbilly3@gmail.com>
2019-02-21 02:55:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,17 +50,13 @@ end
 #####################################################
 
 def runner
-  welcome 
-  initial_round = card_total
-  
+  welcome
+  card_total = initial_round
+
   until card_total > 21
-  card_total = hit?(card_total)
-  display_card_total(card_total)
+    card_total = hit?(card_total)
+    display_card_total(card_total)
 end
+  end_game(card_total)
 
-end_game(card_total)
-  
-  
-end 
-
-  
+end
